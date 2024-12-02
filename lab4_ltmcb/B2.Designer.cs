85887ffe4b579afec854b533351db92a94c72ec5
lmphckhanh <22520635@gm.uit.edu.vn>
2024-12-02 21:31:13 +0700

    update lab4_2

--- lab4_ltmcb/B2.Designer.cs
+++ lab4_ltmcb/B2.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "B2";
+            tbURL = new TextBox();
+            tbPath = new TextBox();
+            btDOWNLOAD = new Button();
+            btOPEN = new Button();
+            btDELETE = new Button();
+            btCLOSE = new Button();
+            rtbContent = new RichTextBox();
+            SuspendLayout();
+            // 
+            // tbURL
+            // 
+            tbURL.Font = new Font("Segoe UI", 12F);
+            tbURL.Location = new Point(38, 27);
+            tbURL.Name = "tbURL";
+            tbURL.Size = new Size(501, 34);
+            tbURL.TabIndex = 0;
+            // 
+            // tbPath
+            // 
+            tbPath.Font = new Font("Segoe UI", 12F);
+            tbPath.Location = new Point(38, 83);
+            tbPath.Name = "tbPath";
+            tbPath.Size = new Size(501, 34);
+            tbPath.TabIndex = 1;
+            // 
+            // btDOWNLOAD
+            // 
+            btDOWNLOAD.Location = new Point(561, 12);
+            btDOWNLOAD.Name = "btDOWNLOAD";
+            btDOWNLOAD.Size = new Size(103, 49);
+            btDOWNLOAD.TabIndex = 3;
+            btDOWNLOAD.Text = "DOWNLOAD";
+            btDOWNLOAD.UseVisualStyleBackColor = true;
+            btDOWNLOAD.Click += btDOWNLOAD_Click;
+            // 
+            // btOPEN
+            // 
+            btOPEN.Location = new Point(561, 76);
+            btOPEN.Name = "btOPEN";
+            btOPEN.Size = new Size(103, 50);
+            btOPEN.TabIndex = 4;
+            btOPEN.Text = "OPEN";
+            btOPEN.UseVisualStyleBackColor = true;
+            btOPEN.Click += btOPEN_Click_1;
+            // 
+            // btDELETE
+            // 
+            btDELETE.Location = new Point(561, 136);
+            btDELETE.Name = "btDELETE";
+            btDELETE.Size = new Size(103, 51);
+            btDELETE.TabIndex = 5;
+            btDELETE.Text = "DELETE";
+            btDELETE.UseVisualStyleBackColor = true;
+            btDELETE.Click += btDELETE_Click_1;
+            // 
+            // btCLOSE
+            // 
+            btCLOSE.Location = new Point(561, 203);
+            btCLOSE.Name = "btCLOSE";
+            btCLOSE.Size = new Size(103, 49);
+            btCLOSE.TabIndex = 6;
+            btCLOSE.Text = "CLOSE";
+            btCLOSE.UseVisualStyleBackColor = true;
+            btCLOSE.Click += btCLOSE_Click_1;
+            // 
+            // rtbContent
+            // 
+            rtbContent.Location = new Point(38, 148);
+            rtbContent.Name = "rtbContent";
+            rtbContent.ScrollBars = RichTextBoxScrollBars.Vertical;
+            rtbContent.Size = new Size(501, 300);
+            rtbContent.TabIndex = 7;
+            rtbContent.Text = "";
+            // 
+            // B2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(675, 463);
+            Controls.Add(rtbContent);
+            Controls.Add(btCLOSE);
+            Controls.Add(btDELETE);
+            Controls.Add(btOPEN);
+            Controls.Add(btDOWNLOAD);
+            Controls.Add(tbPath);
+            Controls.Add(tbURL);
+            Name = "B2";
+            Text = "B2";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tbURL;
+        private TextBox tbPath;
+        private Button btDOWNLOAD;
+        private Button btOPEN;
+        private Button btDELETE;
+        private Button btCLOSE;
+        private RichTextBox rtbContent;
     }
 }
--- lab4_ltmcb/B2.cs
+++ lab4_ltmcb/B2.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,50 @@ namespace lab4_ltmcb
         public B2()
         {
             InitializeComponent();
+        }
+
+        private void btDOWNLOAD_Click(object sender, EventArgs e)
+        {
+            if (tbPath.Text == "" || tbURL.Text == " ")
+                MessageBox.Show("Ô Path hoặc URL đang để trống!", "THÔNG BÁO", MessageBoxButtons.OK);
+
+            WebClient client = new WebClient();
+            client.DownloadFile(tbURL.Text, tbPath.Text);
+            rtbContent.Text = client.DownloadString(tbURL.Text);
+        }
+
+
+
+
+
+       
+        private void btOPEN_Click_1(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "HTML|*.html";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                tbPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void btDELETE_Click_1(object sender, EventArgs e)
+        {
+
+            rtbContent.Text = " ";
+            tbPath.Text = " ";
+            tbURL.Text = " ";
+        }
+
+        private void btCLOSE_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bạn có chắc muốn thoát!", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (MessageBox.Show("Bạn có chắc muốn thoát!", "THÔNG BÁO", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
         }
     }
 }
